a5db9b9d7c4848755edfeee9e02e0a14c9bb28f0
BusinessPug <nikolaj.hering.hansen@gmail.com>
2023-11-17 12:34:27 +0100

    Improved functionality and the addition of comb and bogo sort

--- VisualSort/MainWindow.xaml.cs
+++ VisualSort/MainWindow.xaml.cs
@@ -1,12 +1,6 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace VisualSort
@@ -112,7 +106,8 @@ namespace VisualSort
             shuffleButton.IsEnabled = true;
             sortMethodComboBox.IsEnabled = true;
             speedSlider.IsEnabled = true;
-
+            setsize.IsEnabled = true;
+            arraySizeTextBox.IsEnabled = true;
         }
 
         private void StartSort()
@@ -123,19 +118,16 @@ namespace VisualSort
             speedSlider.IsEnabled = false;
             sortMethodComboBox.IsEnabled = false;
             abortSorting = false;
+            setsize.IsEnabled = false;
+            arraySizeTextBox.IsEnabled = false;
         }
 
-        private async void CurrentDelay()
-        {
-            await Task.Delay(1);
-        }
 
 
         private void StartButton_Click(object sender, RoutedEventArgs e)
         {
             ComboBoxItem selectedItem = ((ComboBoxItem)sortMethodComboBox.SelectedItem);
             string selectedAlgorithm = selectedItem.Content.ToString();
-
             switch (selectedAlgorithm)
             {
                 case "Bubble Sort":
@@ -147,7 +139,15 @@ namespace VisualSort
                 case "Cocktail Shaker Sort":
                     CocktailShakerSort();
                     break;
-                // Add cases for other sorting methods if necessary
+                case "Radix Sort":
+                    RadixSort();
+                    break;
+                case "Comb Sort":
+                    CombSort();
+                    break;
+                case "Bogo Sort":
+                    BogoSort();
+                    break;
                 default:
                     MessageBox.Show("Please select a sorting algorithm.");
                     break;
@@ -159,7 +159,7 @@ namespace VisualSort
 
             StartSort();
             int n = array.Length;
-            int updateFrequency = Math.Max(1, (int)(n / (speedSlider.Value + 1))); // Example: Update UI after every 5% of the array length
+            int updateFrequency = Math.Max(1, (int)(n / (speedSlider.Value + 1)));
 
             for (int i = 0; i < n - 1; i++)
             {
@@ -170,22 +170,22 @@ namespace VisualSort
                         ShuffleArray();
                         DrawArray();
                         ResetUI();
-                        return; // Exit the sorting method
+                        return;
                     }
 
                     if (array[j] > array[j + 1])
                     {
-                        // Swap elements
+
                         int temp = array[j];
                         array[j] = array[j + 1];
                         array[j + 1] = temp;
                     }
 
-                    // Update UI less frequently
                     if (j % updateFrequency == 0)
                     {
+                        Dispatcher.Invoke(() => UpdateCanvas(j, j + 1));
+                        await Task.Delay((int)speedSlider.Value / 2);
                         Dispatcher.Invoke(() => UpdateCanvas(j, j + 1, resetColor: true));
-                        CurrentDelay();
                     }
                 }
             }
@@ -230,11 +230,12 @@ namespace VisualSort
 
                     if (index % updateFrequency == 0 || index == 0)
                     {
+                        Dispatcher.Invoke(() => UpdateCanvas(Math.Max(0, index - 1), index));
+                        await Task.Delay((int)speedSlider.Value / 2);
                         Dispatcher.Invoke(() => UpdateCanvas(Math.Max(0, index - 1), index, resetColor: true));
-                        CurrentDelay();
                     }
                 }
-                
+
             }
             if (!abortSorting)
             {
@@ -275,8 +276,16 @@ namespace VisualSort
 
                     if (i % updateFrequency == 0 || i == n - 2)
                     {
+                        if (abortSorting)
+                        {
+                            ShuffleArray();
+                            DrawArray();
+                            ResetUI();
+                            return;
+                        }
+                        Dispatcher.Invoke(() => UpdateCanvas(i, i + 1));
+                        await Task.Delay((int)speedSlider.Value / 2);
                         Dispatcher.Invoke(() => UpdateCanvas(i, i + 1, resetColor: true));
-                        CurrentDelay();
                     }
                 }
 
@@ -295,8 +304,16 @@ namespace VisualSort
 
                     if (i % updateFrequency == 0 || i == 0)
                     {
+                        if (abortSorting)
+                        {
+                            ShuffleArray();
+                            DrawArray();
+                            ResetUI();
+                            return;
+                        }
+                        Dispatcher.Invoke(() => UpdateCanvas(i, i + 1));
+                        await Task.Delay((int)speedSlider.Value / 2);
                         Dispatcher.Invoke(() => UpdateCanvas(i, i + 1, resetColor: true));
-                        CurrentDelay();
                     }
                 }
             } while (swapped);
@@ -307,6 +324,187 @@ namespace VisualSort
                 ResetUI();
             }
         }
+
+
+        private int GetMax(int[] arr)
+        {
+
+            return arr.Max();
+        }
+        private void CountSort(int[] arr, int n, int exp, int updateFrequency, ref int operationCount)
+        {
+            int[] output = new int[n];
+            int[] count = new int[10];
+
+            for (int i = 0; i < n; i++)
+            {
+                int index = (arr[i] / exp) % 10;
+                count[index]++;
+            }
+
+            for (int i = 1; i < 10; i++)
+                count[i] += count[i - 1];
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+                int index = (arr[i] / exp) % 10;
+                output[count[index] - 1] = arr[i];
+                count[index]--;
+            }
+
+            for (int i = 0; i < n; i++)
+                arr[i] = output[i];
+
+            operationCount++;
+            if (operationCount % updateFrequency == 0)
+            {
+                Dispatcher.Invoke(() => DrawArray());
+
+            }
+        }
+
+        private async void RadixSort()
+        {
+            StartSort();
+
+            int n = array.Length;
+            int m = GetMax(array);
+            int updateFrequency = Math.Max(1, (int)((100 - speedSlider.Value + 1) * (n / 100.0)));
+
+            int operationCount = 0;
+
+            for (int exp = 1; m / exp > 0; exp *= 10)
+            {
+                CountSort(array, n, exp, updateFrequency, ref operationCount);
+
+                if (abortSorting)
+                {
+                    ShuffleArray();
+                    DrawArray();
+                    ResetUI();
+                    return;
+                }
+
+                Dispatcher.Invoke(() => DrawArray());
+                await Task.Delay(1000);
+
+
+            }
+
+            if (!abortSorting)
+            {
+                DrawArray();
+                ResetUI();
+            }
+        }
+
+        private async void CombSort()
+        {
+            StartSort();
+
+            int n = array.Length;
+            int updateFrequency = Math.Max(1, (int)(n / (speedSlider.Value + 100)));
+            int operationCount = 0;
+            float shrinkFactor = 1.3f;
+            int gap = n;
+            bool swapped = true;
+
+            while (gap != 1 || swapped)
+            {
+                gap = (int)(gap / shrinkFactor);
+                if (gap < 1)
+                    gap = 1;
+
+                swapped = false;
+                for (int i = 0; i < n - gap; i++)
+                {
+                    if (array[i] > array[i + gap])
+                    {
+                        int temp = array[i];
+                        array[i] = array[i + gap];
+                        array[i + gap] = temp;
+                        swapped = true;
+
+                        operationCount++;
+                        if (operationCount % updateFrequency == 0)
+                        {
+                            Dispatcher.Invoke(() => UpdateCanvas(i, i + gap));
+                            await Task.Delay((int)speedSlider.Value / 2);
+                            Dispatcher.Invoke(() => UpdateCanvas(i, i + gap, resetColor: true));
+                        }
+                    }
+                }
+
+                if (abortSorting)
+                {
+                    ShuffleArray();
+                    DrawArray();
+                    ResetUI();
+                    return;
+                }
+            }
+
+            if (!abortSorting)
+            {
+                DrawArray();
+                ResetUI();
+            }
+        }
+        private async void BogoSort()
+        {
+            StartSort();
+
+            int n = array.Length;
+            int updateFrequency = Math.Max(1, (int)(n / (speedSlider.Value + 1)));
+            int operationCount = 0;
+
+            Random rnd = new Random();
+
+            while (!IsSorted(array))
+            {
+                if (abortSorting)
+                {
+                    ShuffleArray();
+                    DrawArray();
+                    ResetUI();
+                    return;
+                }
+
+                // Shuffle the array
+                for (int i = 0; i < n; i++)
+                {
+                    int j = rnd.Next(i, n);
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+
+                    operationCount++;
+                    if (operationCount % updateFrequency == 0)
+                    {
+                        Dispatcher.Invoke(() => UpdateCanvas(i, i + 1));
+                        await Task.Delay((int)speedSlider.Value / 2); // Minimal delay for visualization
+                        Dispatcher.Invoke(() => UpdateCanvas(i, i + 1, resetColor: true));
+                    }
+                }
+            }
+
+            if (!abortSorting)
+            {
+                DrawArray(); // Final draw to ensure sorted state is shown
+                ResetUI();
+            }
+        }
+
+        private bool IsSorted(int[] arr)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i - 1] > arr[i])
+                    return false;
+            }
+            return true;
+        }
+
 
         private void UpdateCanvas(int index1, int index2, bool resetColor = false)
         {
